fa06ba634c2a318ff7bccfa6765ccd2c5920bd75
Gulie <gulifeiya@gmail.com>
2022-08-06 15:16:48 -0400

    g

---
+++ src/test/java/com/cydeo/tests/day04_path_jsonpath/SpartansPathMethodTest.java
@@ -0,0 +1,7 @@
+package com.cydeo.tests.day04_path_jsonpath;
+
+public class SpartansPathMethodTest {
+
+
+
+}
---
+++ src/test/java/com/cydeo/util/SpartanTestBase.java
@@ -0,0 +1,13 @@
+package com.cydeo.util;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
+
+public abstract class SpartanTestBase {
+
+    @BeforeAll //@BeforeClass in junit 4
+    public static void setUp() {
+        RestAssured.baseURI = ConfigurationReader.getProperty("spartan.api.url");
+    }
+
+}
